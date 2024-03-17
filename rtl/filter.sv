9da81ee4585455e5fe7514aeaabfe86008a8dc79
Pankaj Nair V M <75352734+PankajNair@users.noreply.github.com>
2024-03-17 13:24:41 +0530

    Update filter.sv

--- rtl/filter.sv
+++ rtl/filter.sv
@@ -22,34 +22,34 @@
 (* use_dsp = "yes" *)
 
 module filter (
-input logic clk, reset,
+input logic clk,
 input logic signed [15:0] inData,
 output logic signed [15:0] outData
 );
 
-logic [15:0] b [0:8];
-logic [15:0] x [0:7];
-logic [31:0] mul [0:8];
-logic [31:0] sumInt [0:2];
-logic [31:0] sum;
+logic signed [15:0] b [0:8];
 
 assign b = {16'h04F6, 16'h0AE4, 16'h1089, 16'h1496, 16'h160F,
             16'h1496, 16'h1089, 16'h0AE4, 16'h04F6};
 
+logic signed [15:0] x [0:8];
+logic signed [31:0] mul [0:8];
+logic signed [32:0] sumInt [0:2];
+logic signed [33:0] sum;
+
 genvar i;
-dff DFF0(clk, reset, inData, x[0]);
+dff DFF(clk, 1'b0, inData, x[0]);
 generate
-    for (i=1; i<8; i++) 
+    for (i=1; i<9; i++) 
         begin: FlipFlop_Block
-            dff DFF(clk, reset, x[i-1], x[i]);
+            dff DFF(clk, 1'b0, x[i-1], x[i]);
         end
 endgenerate
 
-always_comb
+always @(posedge clk)
 begin
-    mul[0] = inData*b[0];
-    for(int i=1; i<5; i++)
-        mul[i] = x[i-1]*b[i];
+    for(int i=0;i<9;i++)
+        mul[i] <= x[i]*b[i];
 end
 
 always @(posedge clk)
@@ -62,11 +62,8 @@ end
 always @(posedge clk)
 begin
     sum <= sumInt[0]+sumInt[1]+sumInt[2];
-end 
-
-always_ff @(posedge clk)
-begin
-    outData <= sum;
 end
+
+assign outData = sum[33:18];
 
 endmodule
